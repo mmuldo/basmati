bf4c0e4063828726c43f862d3efcc98d8959023b
Matt Muldowney <matt.muldowney@gmail.com>
2021-08-26 15:07:13 -0500

    sets displays

---
+++ .local/bin/displays.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+display_list=($(xrandr -q | grep ' connected' | cut -d' ' -f1))
+
+[ ${#display_list[@]} -eq 2 ] && notify-send "hi"
+
+[ ${#display_list[@]} -eq 1 ] &&
+    ~/.screenlayout/single.sh &&
+    notify-send "using single monitor layout" &&
+    exit 0
+
+[ ${#display_list[@]} -eq 2 ] &&
+    ~/.screenlayout/work-setup.sh ${display_list[@]} &&
+    # DISPLAY=:8 xset -dpms s off &&
+    notify-send "using dual monitor layout" &&
+    exit 0
+
+notify-send "no script for current layout" &&
+    exit 1
