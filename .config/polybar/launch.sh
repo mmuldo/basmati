73621a1d440ab1cdce9fee4c54a11744262cb18a
Matt Muldowney <matt.muldowney@gmail.com>
2021-08-27 09:36:08 -0500

    polybar launcher updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# terminate currently launched bars
+#terminate currently launched bars
 killall -q polybar
 sleep 1
 
+displays.sh
+sleep 1
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m polybar -r top &
-	MONITOR=$m polybar -r bottom &
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
 done
 
-# launch bars
-# polybar -r top &
-# polybar -r bottom &
-
-notify-send "bars launched"
+notify-send "polybars launched"
